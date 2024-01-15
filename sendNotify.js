41f751ed2f19848930647e4a2bf185b994b445a4
xw431128 <xw431128@users.noreply.github.com>
2024-01-15 16:52:13 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -21,9 +21,9 @@ let QQ_MODE = 'send';
 
 // =======================================Bark App通知设置区域===========================================
 //此处填你BarkAPP的信息(IP/设备码，例如：https://api.day.app/XXXXXXXX)
-let BARK_PUSH = 'https://api.day.app/i725yfVYY9j7to4eRB4NKL/墨墨分享/GitHub Actions Run';
+let BARK_PUSH = 'https://api.day.app/i725yfVYY9j7to4eRB4NKL/墨墨分享/GitHubActionsRun';
 //BARK app推送铃声,铃声列表去APP查看复制填写
-let BARK_SOUND = '';
+let BARK_SOUND = 'https://api.day.app/i725yfVYY9j7to4eRB4NKL/alarm?sound=minuet';
 
 
 // =======================================telegram机器人通知设置区域===========================================
